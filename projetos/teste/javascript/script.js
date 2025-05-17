7a15fc6a5b908a3312a17a28b452f00f0de203bb
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-05-17 11:45:37 -0300

    Atualizando

---
+++ projetos/teste/javascript/script.js
@@ -0,0 +1,16 @@
+function clickMenu() {
+    if ( itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block'
+        burguer.style.
+    } else {
+        itens.style.display = 'none'
+    }
+}
